6aa6f58ca68573a0a109987248de8fd979d1f283
Rhys Minchin <rhysminchin@hotmail.co.uk>
2022-12-19 13:51:30 +0000

    -completed README -distance.py now works as intended with given CLI input

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 # distance.py
 
+## Notes
+
+The `tabulate` and `haversine` modules may need to be installed prior to running this application. Your IDE may provide the option to install them by right-clicking the module name in the source code. If not, use the IDE terminal and run the commands:
+
+* `pip install tabulate`
+
+* `pip install haversine`
+
+## Usage
+
 Using the IDE terminal, or the command line, usage is as follows:
 
 `distance.py` `[-h]` `[-n [N]]`
 
-Using the `-h` argument provides a help message in the terminal
+Using the `-h` argument provides a help message in the terminal.
 
 The optional `-n` argument takes an integer. If no argument is provided, the default length of `places.csv` is used. If an integer is provided, `N` random places from `places.csv` are selected for use.
 
-The program then outputs the pairs of places and the corresponding air distance between all pairs. Finally, the average air distance and the pair with the distance closest to this average are given as output.
+For example, valid inputs are:
 
+`distance.py -h`
+
+`distance.py -n 4`
+
+`distance.py`
+
+The program then outputs the pairs of places, and the corresponding air distance between all pairs. Finally, the average air distance and the pair with the distance closest to this average are given as output.
--- distance.py
+++ distance.py
@@ -2,11 +2,15 @@
 import argparse
 import csv
 import random
+import haversine as hs
+import tabulate
 
 # define variables
-places = []
+places = {}
 places_iterate = 0
-places_to_use = []
+places_to_use = {}
+pairs = {}
+distances = []
 
 # open the csv file and add data to list
 with open('places.csv', mode='r') as file:
@@ -14,7 +18,7 @@ with open('places.csv', mode='r') as file:
 
     for lines in csvFile:
         if 'Name' not in lines[0]:
-            places.append(lines[0] + " " + lines[1] + " " + lines[2])
+            places[lines[0]] = lines[1] + "," + lines[2]
 
 # configure argparse
 parser = argparse.ArgumentParser(description="Air distance program")
@@ -24,8 +28,8 @@ args = parser.parse_args()
 # if n argument provided, ensure validity and use input provided
 # if not, use entire dataset provided in places.csv
 if args.n is not None:
-    if args.n < 0:
-        places_iterate = 0
+    if args.n < 2:
+        places_iterate = 2
     elif args.n > 10:
         places_iterate = 10
     else:
@@ -38,8 +42,74 @@ if places_iterate == 10:
     places_to_use = places
 else:
     for i in range(places_iterate):
-        place = random.choice(places)
-        places_to_use.append(place)
-        places.remove(place)
+        place = key, val = random.choice(list(places.items()))
+        places_to_use[place[0]] = place[1]
+        places.pop(place[0])
 
-print(places_to_use)
+places_names = list(places_to_use.keys())
+
+# calculate pairs and distances
+x = 0
+y = 1
+total = 0
+while True:
+    try:
+        place1_name = places_names[x]
+    except IndexError:
+        x += 1
+        y = x + 1
+        try:
+            place1_name = places_names[x]
+        except IndexError:
+            # reached end of list
+            break
+
+    try:
+        place2_name = places_names[y]
+    except IndexError:
+        x += 1
+        y = x + 1
+        try:
+            place2_name = places_names[x]
+        except IndexError:
+            # reached end of list
+            break
+
+    # lat, long pairs for haversine function
+    place1_loc = (float(places_to_use[place1_name].split(",")[0]), float(places_to_use[place1_name].split(",")[1]))
+    place2_loc = (float(places_to_use[place2_name].split(",")[0]), float(places_to_use[place2_name].split(",")[1]))
+
+    # calculate haversine distance between the two points
+    distance = hs.haversine(place1_loc, place2_loc)
+    if distance not in distances:
+        pairs[distance] = place1_name + "-" + place2_name
+        distances.append(distance)
+        total += distance
+
+    # next pair
+    y += 1
+
+# sort pairs into order of distance
+pairs_in_order = sorted(pairs)
+sorted_dict = {}
+for i in pairs_in_order:
+    sorted_dict[i] = pairs[i]
+
+# data calculation
+# data is rounded for output but kept precise for calculations
+headers = ['Place 1', 'Place 2', 'Distance (km)']
+data = []
+average = total / len(sorted_dict)
+closest_distance = 999999999999
+closest_pair = ''
+for key, value in sorted_dict.items():
+    data.append([value.split("-")[0], value.split("-")[1], round(key, 3)])
+    if abs(key - average) < closest_distance:
+        closest_distance = abs(key - average)
+        closest_pair = value
+
+# data output
+print(tabulate.tabulate(data, headers, tablefmt='presto'))
+print()
+print("Average distance: " + str(round(average, 3)) + "km, Closest pair: " + closest_pair + " " +
+      str(round(closest_distance, 3)) + " km")
